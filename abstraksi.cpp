68f93e36abaf6109af0773aa59b3cc5448b597af
sabrinakayla <sabrinakuliah10@gmail.com>
2026-05-19 18:28:23 +0700

    memasukan metode untuk mengisi nilai private member

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -4,3 +4,9 @@ using namespace std;
 class AbstraksiKlas {
 private:
     string x, y;
+    public:
+    // method untuk mengisi nilai private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
